99cf5c81c6ecd319696af0904a8a04246090c8c8
Miri-Hwang <72543560+Miri-Hwang@users.noreply.github.com>
2021-01-30 14:08:26 +0900

    feat : Get Result

--- currency_exchage.py
+++ currency_exchage.py
@@ -45,6 +45,7 @@ def take_inputs():
     return {'a_name': a_name, 'a_code': a_code, 'b_name': b_name, 'b_code': b_code, 'amounts': amounts}
 
 
+# Get an Exchange Rate from Country a to Country b in the dict
 def get_rate(dict):
     URL = f"https://transferwise.com/gb/currency-converter/{dict['a_code']}-to-{dict['b_code']}-rate?amount={dict['amounts']}"
     r = requests.get(URL)
@@ -52,10 +53,22 @@ def get_rate(dict):
     calculator = soup.find('div', {'class': 'js-Calculator'}).find('form')
     rate = calculator.find('h3', {
                            'class': 'cc__source-to-target'}).find('span', {'class': 'text-success'}).string
-    print(rate)
+    rate = str(rate.encode('utf-8')).strip('b').replace("'", "")
     return rate
+
+# Get Result
+
+
+def get_result():
+    inputs = take_inputs()
+    a = inputs['a_code']
+    b = inputs['b_code']
+    amounts = float(inputs['amounts'])
+    amounts_format = "{:,.2f}".format(amounts)
+    rate = float(get_rate(inputs))
+    result = "{:,}".format(int(amounts * rate))
+    print(f"{a}{amounts_format} is {b}{result}")
 
 
 start(lists)
-inputs = take_inputs()
-get_rate(inputs)
+get_result()
