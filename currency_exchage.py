0ec649ad2856f343393be624d98cc2ca2ee26fb0
Miri-Hwang <72543560+Miri-Hwang@users.noreply.github.com>
2021-01-30 11:57:50 +0900

    feat : Take Inputs from User
    country a, country b, money amounts to exchange

--- currency_exchage.py
+++ currency_exchage.py
@@ -1,4 +1,4 @@
-from countries import show_countries, extract_name
+from countries import show_countries, extract_name, take_num, extract_code
 
 lists = show_countries()
 
@@ -8,5 +8,22 @@ def start(lists):
     for i in range(len(lists)):
         print(f"# {i} {extract_name(lists[i])}")
 
+# Let user choose country a & b , and amounts of money to exchange.
+
+
+def take_inputs():
+    print("Where are you from? Choose a country by number.\n\n")
+    a = take_num()
+    print(f"{extract_name(lists[a])}\n\n")
+    print("Now choose another country.")
+    b = take_num()
+    print(f"{extract_name(lists[b])}\n\n")
+
+    print(
+        f"How many {extract_code(lists[a])} do you want to convert to {extract_code(lists[b])}?")
+    amounts = take_num()
+    return {'a': a, 'b': b, 'amounts': amounts}
+
 
 start(lists)
+print(take_inputs())
